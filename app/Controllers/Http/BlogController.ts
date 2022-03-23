2ec95befd6556ee2fe9d6e436c435765233ad857
nayOps <raphaelalema@gmail.com>
2022-03-23 01:16:32 +0100

    create posts model

--- app/Controllers/Http/BlogController.ts
+++ app/Controllers/Http/BlogController.ts
@@ -1,4 +1,5 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Post from 'App/Models/Post'
 
 export default class BlogController {
 
@@ -7,7 +8,11 @@ export default class BlogController {
      */
 
     async index ({ view } : HttpContextContract) {
-        return view.render('blog/index')
+
+        const posts = await Post.all()
+      return view.render('blog/index', { 
+          posts
+        })
       
     }
 
